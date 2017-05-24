69953090db145597c6c63b971e25986b0938dc52
Laurence Ruberl <bknd225@gmail.com>
2017-05-24 11:22:36 -0400

    Add things and stuff

---
+++ src/Actions.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Actions.css'
+
+const Actions = ({ thing, removeThing}) => {
+    return(
+        <span className="Actions">
+            <button
+                className="remove"
+                onClick={() => removeThing(thing)}
+            >
+                <i className="fa fa-trash-o"></i>
+            </button>
+        </span>
+    )
+}
+export default Actions
--- src/Thing.js
+++ src/Thing.js
@@ -1,34 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ContentEditable from 'react-contenteditable'
 
+import Actions from './Actions'
 import './Thing.css'
 
-const Thing = ({ thing, saveThing, removeThing }) => {
-  const updateName = (ev) => {
-    thing.name = ev.target.value
-    saveThing(thing)
+class Thing extends Component {
+    componentDidMount(){
+        this.nameInput.htmlEl.focus()
+    }
+
+    updateName = (ev) => {
+        const { thing, saveThing } = this.props
+        thing.name = ev.target.value
+        saveThing(thing)
   }
 
-  return (
-    <li className="Thing">
-      <input type="checkbox" value="on" />
-      <div className="details">
-        <ContentEditable
-          className="name"
-          html={thing.name}
-          onChange={updateName}
-        />
-        <span className="actions">
-          <button
-            className="remove"
-            onClick={() => removeThing(thing)}
-          >
-            <i className="fa fa-trash-o"></i>
-          </button>
-        </span>
-      </div>
-    </li>
-  )
+  blurOnEnter = (ev) => {
+      if (ev.key === 'Enter') {
+          ev.preventDefault()
+          ev.target.blur()
+      }
+  }
+
+  render() {
+    const { thing, removeThing } = this.props
+    return (
+        <li className="Thing">
+            <input type="checkbox" value="on" />
+            <div className="details">
+                <ContentEditable
+                className="name"
+                html={thing.name}
+                onChange={this.updateName}
+                onKeyPress={this.blueOnEnter}
+                ref={input => this.nameInput = input}
+                />
+                <Actions thing={thing} removeThing={removeThing}/>
+            </div>
+        </li>
+    )
+  }
 }
 
 export default Thing
